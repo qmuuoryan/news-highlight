3d3da9bd3589424305d35482185110213927a74b
ryan <you@example.com>
2019-02-07 13:09:16 +0300

    add change

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,21 @@
-#from flask import render_template
-#from app import app
+from flask import render_template
+from . import main
+from ..request import get_sources,get_articles
+from ..models import Source,Article
+# Views
+@main.route('/')
+def index():
+    '''View the index page and its data'''
+    title = 'News Highlight'
+    general_sources = get_sources('general')
+    business_sources = get_sources('business')
+    sports_sources = get_sources('sports')
+    technology_sources = get_sources('technology')
+    return render_template('index.html', title = title, general = general_sources, business = business_sources, sports = sports_sources, technology = technology_sources)
 
-
-@app.route('/News/<int:news_id>')
-def news(news_id):
-
-    '''
-    View news page function that returns the news page and its data
-    '''
-    return render_template('news.html',id = news_id)
+@main.route('/articles/<id>')
+def articles(id):
+    '''View a specific source page and its articles'''
+    articles = get_articles(id)
+    title = f'{id}'
+    return render_template('articles.html',id = id, articles = articles)
